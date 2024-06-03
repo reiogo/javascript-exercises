17aa2631d3df80223e69bf0b42cbc6e66faccb32
reiogo <raogoda2@gmail.com>
2024-06-04 08:38:06 +0900

    Finish 02

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(arg) {
+    let splitString = arg.split("");
+    let res = '';
+    for (let i = 1; i < (arg.length+1); i++) {
+        res += splitString.at(-i);
+
+    }
+    return res;
 
 };
 
